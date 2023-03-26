2d19b8096c9f1d5680e4cde80bf43dde7f8f67ca
arthur-ctm <christmann68@outlook.fr>
2023-03-26 21:57:42 +0100

    first

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,100 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from cars.urls import *
 
-# Create your tests here.
+
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+
+    def test_login_view(self):
+        url = reverse('login')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+        # test with valid credentials
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('car_list'))
+
+        # test with invalid credentials
+        data = {
+            'username': 'invaliduser',
+            'password': 'invalidpassword'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_logout_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        url = reverse('logout')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('index'))
+
+    def test_signup_view(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+        # test with valid form data
+        data = {
+            'username': 'newuser',
+            'password1': 'newpassword',
+            'password2': 'newpassword'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse('car_list'))
+        user = authenticate(username='newuser', password='newpassword')
+        self.assertIsNotNone(user)
+
+        # test with invalid form data
+        data = {
+            'username': 'testuser',
+            'password1': 'testpassword',
+            'password2': 'testpassword'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_change_password_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        url = reverse('change_password')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/password_change.html')
+
+        # test with valid form data
+        data = {
+            'old_password': 'testpassword',
+            'new_password1': 'newpassword',
+            'new_password2': 'newpassword'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('profile'))
+
+        # test with invalid form data
+        data = {
+            'old_password': 'invalidpassword',
+            'new_password1': 'newpassword',
+            'new_password2': 'newpassword'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/password_change.html')
--- cars/tests.py
+++ cars/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+from django.test import TestCase
+from .models import Car, Driver
+
+
+class CarTestCase(TestCase):
+    def setUp(self):
+        # Set up any test data you need here
+        driver = Driver.objects.create(number=22, last_name='Deep', first_name='Johnny', age=34)
+        Car.objects.create(safety=8, colour='red', aero=9, team='Redbull', horses=990, driver=driver)
+
+    def test_model_creation(self):
+        # Test that a model was created successfully
+        obj = Car.objects.get(team='Redbull')
+        self.assertEqual(obj.aero, 9)
+
+    def test_model_deletion(self):
+        # Test that a model can be deleted successfully
+        obj = Car.objects.get(team='Redbull')
+        obj.delete()
+        self.assertRaises(Car.DoesNotExist,Car.objects.get, team='Redbull')
+
+
+class DriverTestCase(TestCase):
+    def setUp(self):
+        # Set up any test data here
+        Driver.objects.create(number=16, last_name='Leclerc', first_name='Charles', age=23)
+
+
+    def test_model_creation(self):
+        # Test that a model was created successfully
+        obj = Driver.objects.get(last_name='Leclerc')
+        self.assertEqual(obj.number, 16)
+
+    def test_model_deletion(self):
+        # Test that a model can be deleted successfully
+        obj = Driver.objects.get(last_name='Leclerc')
+        obj.delete()
+        self.assertRaises(Driver.DoesNotExist, Driver.objects.get, last_name='Leclerc')
